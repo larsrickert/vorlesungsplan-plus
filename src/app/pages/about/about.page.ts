9d7fbdf20ebc215414941a06fdc734628e36a5c2
Lars Rickert <lars.rickert@web.de>
2020-11-29 14:54:44 +0100

    added app version to about page

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilityService } from 'src/app/services/utility/utility.service';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.page.scss'],
 })
 export class AboutPage implements OnInit {
+  version = UtilityService.appVersion;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
