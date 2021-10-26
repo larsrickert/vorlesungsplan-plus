890b18c56d86d1e59e50524752908cb5e19fcf33
Lars Rickert <lars.rickert@web.de>
2021-10-26 12:06:35 +0200

    changed api host

--- src/components/TheCourseSelectModal.vue
+++ src/components/TheCourseSelectModal.vue
@@ -5,6 +5,7 @@
     <IonContent :fullscreen="true">
       <div class="page__content">
         <p>{{ t('global.placeholder') }}</p>
+        <pre>{{ courses }}</pre>
       </div>
     </IonContent>
   </IonPage>
@@ -12,10 +13,18 @@
 
 <script lang="ts" setup>
 import { IonContent, IonPage, modalController } from '@ionic/vue';
+import { ref } from 'vue';
 import { useI18n } from 'vue-i18n';
+import axiosInstance from '../axios';
 import AppHeader from '../components/AppHeader.vue';
 
 const { t } = useI18n();
+
+const courses = ref<string[]>([]);
+
+axiosInstance.get<string[]>('courses').then((response) => {
+  courses.value = response.data;
+});
 </script>
 
 <style lang="scss" scoped></style>
