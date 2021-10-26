f627a95d91fbdb112c943e6c8a7e2b2f55b84209
Lars Rickert <lars.rickert@web.de>
2021-10-26 13:40:29 +0200

    added lectures to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { createPinia, defineStore } from 'pinia';
+import axiosInstance from '../axios';
 import { isProduction } from '../configs';
 import { loggerPlugin } from '../store/plugins/logger';
 
@@ -7,6 +8,40 @@ if (!isProduction) pinia.use(loggerPlugin);
 
 export const useStore = defineStore('main', {
   state() {
-    return {};
+    return {
+      lectures: [] as MergedLecture[],
+    };
+  },
+  actions: {
+    async fetchLectures(courses: string[]) {
+      const lectures: Lecture[] = [];
+
+      for (const course of courses) {
+        const { data } = await axiosInstance.get<ApiLecture[]>(`lectures/${course}`);
+        data.forEach((lecture) => {
+          lectures.push({
+            ...lecture,
+            start: new Date(lecture.start),
+            end: new Date(lecture.end),
+          });
+        });
+      }
+
+      this.lectures = mergeAndSortSameLectures(lectures);
+    },
+    removeLectures() {
+      if (this.lectures.length > 0) this.lectures = [];
+    },
+  },
+  getters: {
+    upcomingLectures(): MergedLecture[] {
+      return this.lectures.filter((lecture) => lecture.end.getTime() > Date.now());
+    },
+    presenceLectures(): MergedLecture[] {
+      return this.lectures.filter((lecture) => lecture.room);
+    },
+    exams(): MergedLecture[] {
+      return this.lectures.filter((lecture) => lecture.name.toLowerCase().includes('klausur'));
+    },
   },
 });
