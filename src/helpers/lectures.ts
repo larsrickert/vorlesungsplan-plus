a34164c872f7d12ae416778875012c840a1f16ad
Lars Rickert <67898185+larsrickert@users.noreply.github.com>
2021-11-03 11:25:18 +0100

    fixed lecture merging for multiple courses

--- src/helpers/lectures.ts
+++ src/helpers/lectures.ts
@@ -27,13 +27,13 @@ export function mergeAndSortSameLectures(lectures: Lecture[]): MergedLecture[] {
   const merged: MergedLecture[] = [];
 
   for (let i = 0; i < sorted.length; i++) {
-    if (merged.length && i + 1 < sorted.length && isSameLectureContent(sorted[i], sorted[i + 1])) {
+    if (i + 1 < sorted.length && isSameLectureContent(sorted[i], sorted[i + 1])) {
       // lectures have same content, two cases:
-      const lastMerged = merged[merged.length - 1];
+      const lastMerged = merged.length ? merged[merged.length - 1] : null;
       const a = sorted[i];
       const b = sorted[i + 1];
 
-      if (!lastMerged.uids.includes(a.uid)) {
+      if (!lastMerged?.uids.includes(a.uid)) {
         // 1: first was not already added to merged
         merged.push({
           uids: [a.uid, b.uid],
@@ -52,6 +52,7 @@ export function mergeAndSortSameLectures(lectures: Lecture[]): MergedLecture[] {
 
       i++;
     } else {
+      // current (i) and next (i+1) lecture dont have the same content
       merged.push({
         uids: [sorted[i].uid],
         courses: [sorted[i].course],
