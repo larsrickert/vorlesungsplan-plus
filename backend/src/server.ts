9343238a2ae9a84c08236aee15ec3df8feaa4653
Lars Rickert <dev@lars-rickert.de>
2022-03-19 20:10:31 +0100

    fix: Logger middleware not called

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,22 +9,22 @@ import { createLogger } from './utils/logger';
 
 const app = express();
 app.use(cors(config.app.cors));
-app.use(
+
+export const logger = createLogger();
+export const router = express.Router();
+app.use(router);
+
+router.use(
   '/docs',
   swaggerUi.serve,
   swaggerUi.setup(swaggerJSDoc(config.app.swagger))
 );
 
-export const logger = createLogger();
-
-// routes have to been registered before middleware
-export const router = express.Router();
-require('./routes');
-app.use(router);
-
 // middleware
 router.use(loggerMiddleware);
 router.use(errorMiddleware);
+
+require('./routes');
 
 app.listen(config.app.port, () =>
   console.log(`Running on http://localhost:${config.app.port}`)
