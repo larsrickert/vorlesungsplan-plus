0f4a0494c5baf7ce2ffb5b05bdec42b445b5c0cd
Lars Rickert <dev@lars-rickert.de>
2022-03-20 18:42:20 +0100

    refactor: renamed ios widget

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -57,7 +57,7 @@ const config: IConfig = {
     ios: {
       widget: {
         version: '4.0.0',
-        filePath: `${publicPath}mosbach-vorlesungsplan-widget-v4-0-0.scriptable`,
+        filePath: `${publicPath}vorlesungsplan-plus-ios-widget-v4-0-0.scriptable`,
       },
     },
   },
